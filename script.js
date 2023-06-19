8a9231d1f8cbc6aa23b8c1c753de51a9ef1d9b76
Harshal Kamdi <118383371+harshal1714@users.noreply.github.com>
2023-06-20 00:46:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 function getFormvalue() {
     //Write your code here
+ // Retrieve the form element
+  var form = document.getElementById('form1');
+
+  // Retrieve the values of the first and last name fields
+  var firstName = form.elements['fname'].value;
+  var lastName = form.elements['lname'].value;
+
+  // Display the values using alert()
+  alert(firstName + ' ' + lastName);
 
 }
